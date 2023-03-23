19641e6144b1c709dcc0f4b59e8d969d45ac5372
Nadine <nadinehamdy128@gmail.com>
2023-03-23 22:09:00 +0200

    adding to add product

--- Project/JS/nav.js
+++ Project/JS/nav.js
@@ -8,7 +8,7 @@ const creatNav = () => {
                 <input type="text" name="search-bar" placeholder="Search for a shop, product !">
                 <button class="search-btn">Search</button>
                 <a href=""><img src="photos/user2.0.jpg" alt="user"></a>
-                <a href=""><img src="photos/shoppingCart.jpg" alt="cart"></a>
+                <a href=shoppingcart.html""><img src="photos/shoppingCart.jpg" alt="cart"></a>
             </div>
         </div>
     </div>
