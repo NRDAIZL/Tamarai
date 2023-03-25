f93939627dc84abb746c3f41677632237d5fe818
solsol <shahdahmed7203@gmail.com>
2023-03-25 20:06:42 +0200

    changes

--- JS/Functions.js
+++ JS/Functions.js
@@ -1,76 +1,86 @@
+// class formValidator 
+// {
+//     constructor(form, fields) {
+//         this.form = form
+//         this.fields = fields
+//     }
+//     initialize() {
+//         this.validateOnSubmit()
+//     }
+//     validateOnSubmit() {
+//         let self = this;
+//         this.form.addEventListener('submit', event => {
+//             event.preventDefault()
+//             self.fields.forEach(field => {
+//                 const input = document.getElementById(field);
+//                 self.validateFields(input)
+//             })
+//         })
 
-class formValidator
+//     }
+//     validateFields(field) {
+//         if (field.value.trim === "") 
+//         {
+//             document.getElementById('error-message').innerHTML="Cannot be blank";
+//             return false;
+//         }
+        // else {
+        //     this.setStatus(field, null,"success")
+
+        // }
+        // if (field.type === "email") {
+        //     const re =  /\S+@\S+.\S+/
+        //     if(re.test(field.value)) {
+        //         this.setStatus(field, null, "Success")
+        //     }
+        //     else {
+        //         this.setStatus(field, "Please enter a valid Email", "Error")
+        //     }
+        // }
+        // if(field.id === "password") {i
+        //     // const passwordField = this.form.querySelector('#password')
+        //     if(field.value.trim === "") {
+        //         this.setStatus(field, "Password required", "Error")
+        //     }
+        //     // else if(field.value!=passwordField.value){
+        //     //     this.setStatus(field,"Password doesn't match", "Error")
+        //     }
+        //     else {
+        //         this.setStatus(field, null, "Success")
+        //     }
+//         }
+// }
+// const form = document.querySelector('.form')
+// const fields = ["username", "email", "password", "confirmpassword"]
+// const validator = new formValidator(form, fields)
+// validator.initialize()
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const email = document.getElementById('email');
+const dob = document.getElementById('date-of-birth');
+form.addEventListener('submit',(e)=>{
+
+    e.preventDefault();
+    
+    validateFields();
+    });
+function validateFields()
 {
-   constructor(form, fields)
-   {
-      this.form=form;
-      this.fields=fields;
-   }
-   initialize()
-   {
-      this.validateOnSubmit();
-   }
-   validateOnSubmit()
-   {
-      let self = this;
-      this.form.addEventListener('submit', event=>
-      {
-         event.preventDefault();
-         self.fields.array.forEach(element => {
-            const input = document.querySelector(`#${field}`);
-            self.validateFields(input);
-         });
-      })
-   }
-   validateFields(field)
-   {
-      if(field.value.trim == "")
-      {
-         this.setStatus(field, field.previousElementSibiling.innerText + 'Cannot be blank', "Error")
-      }
-      else {
-         this.setStatus(field, null,"success")
-     }
-     if (field.type === "email") {
-         const re =  /\S+@\S+.\S+/
-         if(re.test(field.value)) {
-             this.setStatus(field, null, "Success")
+    const name=username.value.trim();
+    const passes=password.value.trim();
+    const emai=email.value;
+    const deb=dob.value;
 
-         }
-         else {
-             this.setStatus(field, "Please enter a valid Email", "Error")
-         }
-     }
-     if(field.id === "confirmpassword") {i
-         const passwordField = this.form.querySelector('#password')
-         if(field.value.trim === "") {
-             this.setStatus(field, "Password confirmation required", "Error")
-         }
-         else if(field.value!=passwordField.value){
-             this.setStatus(field,"Password doesn't match", "Error")
-         }
-         else {
-             this.setStatus(field, null, "Success")
-         }
-     }
-
-   }
-   setStatus(field, message, status) {
-      const errorMsg = field.parentEleemnt.querySelector('.error-message')
-      if (status === "Success") {
-          if(errorMsg) {
-              errorMsg.innerText = ""
-          }
-          field.classList.remove('input-error')
-      }
-      if (status === "error") {
-          field.parentEleemnt.querySelector('.error-message').innerText = message
-          field.classList.add('input-error')
-      }
-  }
-
+  if(name =='')
+{
+  document.getElementById('Uerror').innerHTML="Username Cannot Be Empty";
+  return false;
 }
-const form = document.querySelector('.form')
-const fields = ["username", "email", "password", "confirmpassword"]
-const validator = new formValidator(form, fields)
-validator.initialize()
+else
+{
+    document.getElementById('Uerror').innerHTML="";
+    return true;
+}
+}
