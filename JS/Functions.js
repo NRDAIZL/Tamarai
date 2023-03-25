ac9ee2e8c8cde266987361261e69d338796ae4bf
solsol <shahdahmed7203@gmail.com>
2023-03-25 21:32:43 +0200

    validations

--- JS/Functions.js
+++ JS/Functions.js
@@ -1,86 +1,46 @@
-// class formValidator 
-// {
-//     constructor(form, fields) {
-//         this.form = form
-//         this.fields = fields
-//     }
-//     initialize() {
-//         this.validateOnSubmit()
-//     }
-//     validateOnSubmit() {
-//         let self = this;
-//         this.form.addEventListener('submit', event => {
-//             event.preventDefault()
-//             self.fields.forEach(field => {
-//                 const input = document.getElementById(field);
-//                 self.validateFields(input)
-//             })
-//         })
-
-//     }
-//     validateFields(field) {
-//         if (field.value.trim === "") 
-//         {
-//             document.getElementById('error-message').innerHTML="Cannot be blank";
-//             return false;
-//         }
-        // else {
-        //     this.setStatus(field, null,"success")
-
-        // }
-        // if (field.type === "email") {
-        //     const re =  /\S+@\S+.\S+/
-        //     if(re.test(field.value)) {
-        //         this.setStatus(field, null, "Success")
-        //     }
-        //     else {
-        //         this.setStatus(field, "Please enter a valid Email", "Error")
-        //     }
-        // }
-        // if(field.id === "password") {i
-        //     // const passwordField = this.form.querySelector('#password')
-        //     if(field.value.trim === "") {
-        //         this.setStatus(field, "Password required", "Error")
-        //     }
-        //     // else if(field.value!=passwordField.value){
-        //     //     this.setStatus(field,"Password doesn't match", "Error")
-        //     }
-        //     else {
-        //         this.setStatus(field, null, "Success")
-        //     }
-//         }
-// }
-// const form = document.querySelector('.form')
-// const fields = ["username", "email", "password", "confirmpassword"]
-// const validator = new formValidator(form, fields)
-// validator.initialize()
-
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const email = document.getElementById('email');
-const dob = document.getElementById('date-of-birth');
-form.addEventListener('submit',(e)=>{
-
-    e.preventDefault();
-    
-    validateFields();
-    });
-function validateFields()
+function ValidateName(field)
 {
-    const name=username.value.trim();
-    const passes=password.value.trim();
-    const emai=email.value;
-    const deb=dob.value;
-
-  if(name =='')
-{
-  document.getElementById('Uerror').innerHTML="Username Cannot Be Empty";
-  return false;
-}
-else
-{
-    document.getElementById('Uerror').innerHTML="";
+  if (field === '')
+  {
+    document.getElementById('Uerror').innerHTML='Enter a valid username';
+    document.getElementById('Uerror').style.display='block';
+    return false;
+  }
+  else
+  {
+    document.getElementById('Uerror').innerHTML='';
+    document.getElementById('Uerror').style.display='none';
     return true;
+  }
+}   
+function ValidatePass(field)
+{
+  if (field === '')
+  {
+    document.getElementById('Perror').innerHTML='Enter a valid password';
+    document.getElementById('Perror').style.display='block';
+    return false;
+  }
+  else
+  {
+    document.getElementById('Perror').innerHTML='';
+    document.getElementById('Perror').style.display='none';
+    return true;
+  }
 }
+function Validation(form)
+{
+  let failing ='';
+  failing&= ValidateName(form.username.value.trim());
+  failing&= ValidatePass(form.password.value.trim());
+  
+  if(failing)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
+
