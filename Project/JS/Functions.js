6213524a3a2ee76b5dde07ecb867cf8ec0b43a77
solsol <shahdahmed7203@gmail.com>
2023-03-25 16:20:23 +0200

    yarab

--- Project/JS/Functions.js
+++ Project/JS/Functions.js
@@ -1,31 +1,76 @@
-document.forms['former'].onsubmit = function(event)
+
+class formValidator
 {
-   if(this.admin-code.value.trim() === ""){
-     document.querySelector(".Aerror").innerHTML = "Please enter a valid code";
-     document.querySelector(".Aerror").style.display = "block";
-     event.preventDefault();
-     return false;
+   constructor(form, fields)
+   {
+      this.form=form;
+      this.fields=fields;
    }
+   initialize()
+   {
+      this.validateOnSubmit();
+   }
+   validateOnSubmit()
+   {
+      let self = this;
+      this.form.addEventListener('submit', event=>
+      {
+         event.preventDefault();
+         self.fields.array.forEach(element => {
+            const input = document.querySelector(`#${field}`);
+            self.validateFields(input);
+         });
+      })
+   }
+   validateFields(field)
+   {
+      if(field.value.trim == "")
+      {
+         this.setStatus(field, field.previousElementSibiling.innerText + 'Cannot be blank', "Error")
+      }
+      else {
+         this.setStatus(field, null,"success")
+     }
+     if (field.type === "email") {
+         const re =  /\S+@\S+.\S+/
+         if(re.test(field.value)) {
+             this.setStatus(field, null, "Success")
 
-   if(this.username.value.trim() === ""){
-      document.querySelector(".Uerror").innerHTML = "Please enter a username";
-      document.querySelector(".Uerror").style.display = "block";
-      event.preventDefault();
-      return false;
-   }
+         }
+         else {
+             this.setStatus(field, "Please enter a valid Email", "Error")
+         }
+     }
+     if(field.id === "confirmpassword") {i
+         const passwordField = this.form.querySelector('#password')
+         if(field.value.trim === "") {
+             this.setStatus(field, "Password confirmation required", "Error")
+         }
+         else if(field.value!=passwordField.value){
+             this.setStatus(field,"Password doesn't match", "Error")
+         }
+         else {
+             this.setStatus(field, null, "Success")
+         }
+     }
 
-   if(this.email.value.trim() === "" || theForm.email.value.length < 10){
-      document.querySelector(".Eerror").innerHTML = "Please enter an email";
-      document.querySelector(".Eerror").style.display = "block";
-      event.preventDefault();
-      return false;
    }
-  
-   if(this.password.value.trim() === "" || this.password.value.length < 6){
-      document.querySelector(".Perror").innerHTML = "Please enter a password";
-      document.querySelector(".Perror").style.display = "block";
-      event.preventDefault();
-      return false;
-   }
+   setStatus(field, message, status) {
+      const errorMsg = field.parentEleemnt.querySelector('.error-message')
+      if (status === "Success") {
+          if(errorMsg) {
+              errorMsg.innerText = ""
+          }
+          field.classList.remove('input-error')
+      }
+      if (status === "error") {
+          field.parentEleemnt.querySelector('.error-message').innerText = message
+          field.classList.add('input-error')
+      }
+  }
 
 }
+const form = document.querySelector('.form')
+const fields = ["username", "email", "password", "confirmpassword"]
+const validator = new formValidator(form, fields)
+validator.initialize()
