90531f93c2b6589000b5b1b083f50cf793b849e8
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-03-30 16:30:20 +0200

    linking functions with edit profile

--- JS/Functions.js
+++ JS/Functions.js
@@ -1,62 +1,50 @@
-function ValidateName(field)
-{
-  if (field == '')
-  {
-    document.getElementById('Uerror').innerHTML='Enter a valid username';
-    document.getElementById('Uerror').style.display='block';
+function ValidateName(field) {
+  if (field == '') {
+    document.getElementById('Uerror').innerHTML = 'Enter a valid username';
+    document.getElementById('Uerror').style.display = 'block';
     return false;
   }
-  else
-  {
-    document.getElementById('Uerror').innerHTML='';
-    document.getElementById('Uerror').style.display='none';
-    return true;
-  }
-}   
-function ValidatePass(field)
-{
-  if (field == '')
-  {
-    document.getElementById('Perror').innerHTML='Enter a valid password';
-    document.getElementById('Perror').style.display='block';
-    return false;
-  }
-  else
-  {
-    document.getElementById('Perror').innerHTML='';
-    document.getElementById('Perror').style.display='none';
+  else {
+    document.getElementById('Uerror').innerHTML = '';
+    document.getElementById('Uerror').style.display = 'none';
     return true;
   }
 }
-function ValidateMail(field)
-{
-  if (field == '')
-  {
-    document.getElementById('Eerror').innerHTML='Enter a valid email';
-    document.getElementById('Eerror').style.display='block';
+function ValidatePass(field) {
+  if (field == '') {
+    document.getElementById('Perror').innerHTML = 'Enter a valid password';
+    document.getElementById('Perror').style.display = 'block';
     return false;
   }
-  else
-  {
-    document.getElementById('Eerror').innerHTML='';
-    document.getElementById('Eerror').style.display='none';
+  else {
+    document.getElementById('Perror').innerHTML = '';
+    document.getElementById('Perror').style.display = 'none';
+    return true;
+  }
+}
+function ValidateMail(field) {
+  if (field == '') {
+    document.getElementById('Eerror').innerHTML = 'Enter a valid email';
+    document.getElementById('Eerror').style.display = 'block';
+    return false;
+  }
+  else {
+    document.getElementById('Eerror').innerHTML = '';
+    document.getElementById('Eerror').style.display = 'none';
     return true;
   }
 }
 
-function Validation(form)
-{
-  let failing ='';
-  failing&= ValidateName(form.username.value.trim());
-  failing&= ValidatePass(form.password.value.trim());
-  failing&= ValidateMail(form.email.value.trim());
-  
-  if(failing)
-  {
+function Validation(form) {
+  let failing = '';
+  failing &= ValidateName(form.username.value.trim());
+  failing &= ValidatePass(form.password.value.trim());
+  failing &= ValidateMail(form.email.value.trim());
+
+  if (failing) {
     return true;
   }
-  else
-  {
+  else {
     return false;
   }
 }
