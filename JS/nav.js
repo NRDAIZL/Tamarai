92dee8d3218dfae05b91a4032c39a4523cf2a506
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-04-06 12:22:29 +0200

    Styling the navigation bar in a separate file

--- JS/nav.js
+++ JS/nav.js
@@ -7,11 +7,11 @@ const creatNav = () => {
                 <div class="search">
                     <input type="text" class="search-bar" placeholder="Search for a shop, product !">
                     <button class="search-btn">Search</button>
+                    <a href=""><img src="photos/user2.0.jpg" alt="user"></a>
+                    <a href="shopping cart.html"><img src="photos/shoppingCart.jpg" alt="cart"></a>
+                    <a href="wishList.html"><img src="photos/wishlist.png" alt="Wishlist"></a>
+                    <a href="chat.html"><img src="photos/chat.png" alt="chat"></a>
                 </div>
-                 <a href=""><img src="photos/user2.0.jpg" alt="user"></a>
-                 <a href="shopping cart.html"><img src="photos/shoppingCart.jpg" alt="cart"></a>
-                 <a href="wishList.html"><img src="photos/wishlist.png" alt="Wishlist"></a>
-                 <a href="chat.html"><img src="photos/chat.png" alt="chat"></a>
             </div>
         </div>
         <ul class="links">
