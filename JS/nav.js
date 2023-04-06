6dda30b91df0abdafb24c9c3094fc6d9cd613b59
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-04-06 12:14:03 +0200

    do not delete

--- JS/nav.js
+++ JS/nav.js
@@ -1,28 +1,28 @@
 const creatNav = () => {
     let nav = document.querySelector('.navbar');
     nav.innerHTML = `
-    <div class="nav">
-        <img style="width: 50px;margin-right: 2px;" src="photos/icons8-lotus-100.png" alt="Tamarii Logo">
-        <div class="nav_items">
-            <div class="search">
-                <input type="text" name="search-bar" placeholder="Search for a shop, product !">
-                <button class="search-btn">Search</button>
-                <a href=""><img src="photos/user2.0.jpg" alt="user"></a>
-                <a href="shopping cart.html"><img src="photos/shoppingCart.jpg" alt="cart"></a>
-                <a href="wishList.html"><img src="photos/wishlist.png" alt="Wishlist"></a>
-                <a href="chat.html"><img src="photos/chat.png" alt="chat"></a>
+        <div class="nav">
+            <img src="photos/icons8-lotus-100.png" class="brand-logo" alt="Tamarii Logo">
+            <div class="nav_items">
+                <div class="search">
+                    <input type="text" class="search-bar" placeholder="Search for a shop, product !">
+                    <button class="search-btn">Search</button>
+                </div>
+                 <a href=""><img src="photos/user2.0.jpg" alt="user"></a>
+                 <a href="shopping cart.html"><img src="photos/shoppingCart.jpg" alt="cart"></a>
+                 <a href="wishList.html"><img src="photos/wishlist.png" alt="Wishlist"></a>
+                 <a href="chat.html"><img src="photos/chat.png" alt="chat"></a>
             </div>
         </div>
-    </div>
-    <ul class="links">
-        <li class="link-item"><a href="" class="link">Home</a></li>
-        <li class="link-item"><a href="" class="link">Craft</a></li>
-        <li class="link-item"><a href="" class="link">Women</a></li>
-        <li class="link-item"><a href="" class="link">Men</a></li>
-        <li class="link-item"><a href="" class="link">kids</a></li>
-        <li class="link-item"><a href="" class="link">Accessories</a></li>
-        <li class="link-item"><a href="" class="link">Gifts</a></li>
-    </ul>
+        <ul class="links">
+            <li class="link-item"><a href="" class="link">Home</a></li>
+            <li class="link-item"><a href="" class="link">Craft</a></li>
+            <li class="link-item"><a href="" class="link">Women</a></li>
+            <li class="link-item"><a href="" class="link">Men</a></li>
+            <li class="link-item"><a href="" class="link">kids</a></li>
+            <li class="link-item"><a href="" class="link">Accessories</a></li>
+            <li class="link-item"><a href="" class="link">Gifts</a></li>
+        </ul>
     `;
 }
 
