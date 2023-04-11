5321f35d9b296ed3cf9365879eec4f8a2b417651
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2023-04-11 11:55:00 +0200

    decreasing function

--- shopping cart.js
+++ shopping cart.js
@@ -41,3 +41,36 @@ let items2 =Number(document.querySelector('.count2').innerText);
 let total_items =Number(items1+items2);
 document.querySelector('.items').innerText=total_items;
 }
+function decreasing(){
+    
+    let counter =document.querySelector('.count' ).innerText;
+    let price= document.querySelector('.amount' ).innerText;
+  if (counter>0||counter==0){
+     
+    if (counter>0){
+    let price= document.querySelector('.amount' ).innerText;
+      let pfor_one_product=price/counter;
+     counter--;
+      
+
+     document.querySelector('.count' ).innerText=counter;
+     document.querySelector('.amount' ).innerText=counter*pfor_one_product 
+    } 
+    else if(counter ==0)
+    {
+        price =0;
+        document.querySelector('.amounst' ).innerText=0;
+    }
+ 
+}
+var  Sub_Total= Number(document.querySelector('.amount').innerText);
+var Sub_Total2=Number(document.querySelector('.amount2').innerText);
+var Total= Sub_Total+Sub_Total2; 
+document.querySelector('.total-amount').innerHTML=Total;
+
+ //# of items
+ let items1 =Number(document.querySelector('.count').innerText);
+let items2 =Number(document.querySelector('.count2').innerText);
+let total_items =Number(items1+items2);
+document.querySelector('.items').innerText=total_items;
+}
