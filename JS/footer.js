8693694c537745d2af290f9ed13396fbed0d854d
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-04-12 23:00:57 +0200

    Making footer.js
    Separating footer to use it in all pages without repetition like navigation bar

---
+++ JS/footer.js
@@ -0,0 +1,64 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+    <div class="footer-content">
+        <img src="photos/lotus.ico" class="logo" alt="Thamaraii Logo">
+        <div class="footer-ul-container">
+            <ul class="category">
+                <li class="category-title">Men</li>
+                <li><a href="" class="footer-link">T-shirts</a></li>
+                <li><a href="" class="footer-link">SweatShirts</a></li>
+                <li><a href="" class="footer-link">Shirts</a></li>
+                <li><a href="" class="footer-link">Jeans</a></li>
+                <li><a href="" class="footer-link">Trousers</a></li>
+                <li><a href="" class="footer-link">Shoes</a></li>
+                <li><a href="" class="footer-link">Casuals</a></li>
+                <li><a href="" class="footer-link">Formals</a></li>
+                <li><a href="" class="footer-link">Sportive</a></li>
+            </ul>
+            <ul class="category">
+                <li class="category-title">Women</li>
+                <li><a href="#" class="footer-link">T-shirts</a></li>
+                <li><a href="#" class="footer-link">SweatShirts</a></li>
+                <li><a href="" class="footer-link">Shirts</a></li>
+                <li><a href="" class="footer-link">Jeans</a></li>
+                <li><a href="" class="footer-link">Trousers</a></li>
+                <li><a href="" class="footer-link">Shoes</a></li>
+                <li><a href="" class="footer-link">Casuals</a></li>
+                <li><a href="" class="footer-link">Formals</a></li>
+                <li><a href="" class="footer-link">Sportive</a></li>
+            </ul>
+            <!-- <ul class="category">
+                <li class="category-title">Get Help</li>
+                <li><a href="#" class="footer-link">Help Center</a></li>
+                <li><a href="#" class="footer-link">Privacy Policy</a></li>
+                <li><a href="#" class="footer-link">Terms</a></li>
+                <li><a href="#" class="footer-link">Login</a></li>
+            </ul> -->
+        </div>
+    </div>
+    <p class="footer-title">Tamarii</p>
+    <p class="info">We could not have reached this milestone of achievement without you.</p>
+    <p class="info">Support Emails - help@tamarai.com, support@tamarai.com</p>
+    <p class="info">Telephone - 0123456789</p>
+    <div class="footer-social-container">
+        <div>
+            <a href="#" class="social-link">Terms & Services</a>
+            <a href="#" class="social-link">Privacy & Policy</a>
+        </div>
+        <div>
+            <div class="social-media">
+                <a href="#"><i class="bx bxl-linkedin-square"></i></a>
+                <a href="#"><i class="bx bxl-facebook-square"></i></a>
+                <a href="#"><i class="bx bxl-instagram-alt"></i></a>
+                <a href="#"><i class="bx bxl-github"></i></a>
+            </div>
+        </div>
+    </div>
+    <p class="footer-credit">Thamaraii, Best Online store.</p>
+    `;
+
+}
+
+createFooter();
