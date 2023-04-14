a2f6921c92f9b4578b60130652a0d4bb5a147ad8
solsol <shahdahmed7203@gmail.com>
2023-04-14 21:19:52 +0200

    links page shghala allah akbr

--- JS/Functions.js
+++ JS/Functions.js
@@ -59,7 +59,7 @@ function USValidation(form)
 }
 function ASValidation(form) 
 {
-  if (ValidateName(form.username.value.trim()) && ValidatePass(form.password.value.trim()) &&ValidateCode(form.admin-code.value.trim())) 
+  if (ValidateCode(form.admin-code.value.trim()) && ValidateName(form.username.value.trim()) && ValidatePass(form.password.value.trim()) ) 
   {
     return true;
   }
